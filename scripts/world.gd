52f3113891c4384bb42839d8703c08a96529b993
KartikeyCode <codekartikey@gmail.com>
2025-03-08 14:07:06 +0530

    add protobuff addon

--- scripts/world.gd
+++ scripts/world.gd
@@ -3,11 +3,17 @@ extends Node2D
 @onready var welcome_sign_label: Label = $welcomeSign/Sign/popup/base/NinePatchRect/MarginContainer/VBoxContainer/Label
 @onready var left_sign_label: Label = $leftSign/Sign/popup/base/NinePatchRect/MarginContainer/VBoxContainer/Label
 
+const packets := preload("res://scripts/packets.gd")
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	left_sign_label.text = "North: Playground \n East: StudyHall"
-							  
+	var packet := packets.Packet.new()
+	packet.set_sender_id(69)
+	var chat_msg := packet.new_chat()
+	chat_msg.set_msg("Hello!")
+	push_warning(packet)							  
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
