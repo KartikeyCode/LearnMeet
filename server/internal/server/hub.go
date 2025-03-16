6d9f700d26cbe8d64bc33abd120107f4525b493d
KartikeyCode <codekartikey@gmail.com>
2025-03-17 04:58:03 +0530

    create gorilla websocket

--- server/internal/server/hub.go
+++ server/internal/server/hub.go
@@ -1,10 +1,10 @@
 package server
 
-import {
+import (
 	"log"
 	"net/http"
 	"server/pkg/packets"
-}
+)
 
 type ClientInterfacer interface {
 
@@ -23,10 +23,10 @@ type ClientInterfacer interface {
 	//puts data from another client into the write pump
 	SocketSendAs(message packets.Msg, senderId uint64)
 
-	// Forward message to another client for processing 
-	PassToPeer(message packets.Msg, senderId uint64)
+	// Forward message to another client for processing
+	PassToPeer(message packets.Msg, peerId uint64)
 
-	// Forward message to all other clients for processing 
+	// Forward message to all other clients for processing
 	Broadcast(message packets.Msg)
 
 	// close client conncetion and cleanup
@@ -37,14 +37,12 @@ type ClientInterfacer interface {
 
 	// pump data from client to socket
 	WritePump()
-
 }
 
-
-//the hub is the central point of communication between all connected clients
+// the hub is the central point of communication between all connected clients
 type Hub struct {
-	Clients map[uint64]ClientInterfacer 
-	
+	Clients map[uint64]ClientInterfacer
+
 	//packets in this channel will be processed by all connected clients except the sender
 	BroadcastChan chan *packets.Packet
 
@@ -52,49 +50,47 @@ type Hub struct {
 	RegisterChan chan ClientInterfacer
 
 	// clients in this channel will be unregistered from the hub
-	UnregisteredChan chan ClientInterfacer
-
-
+	UnregisterChan chan ClientInterfacer
 }
 
 func NewHub() *Hub {
-	return &hub{
-		Clients: make(map[uint64]ClientInterfacer),
-		BroadcastChan: make(chan *packets.Packet),
-		RegisterChan: make(chan ClientInterfacer),
+	return &Hub{
+		Clients:        make(map[uint64]ClientInterfacer),
+		BroadcastChan:  make(chan *packets.Packet),
+		RegisterChan:   make(chan ClientInterfacer),
 		UnregisterChan: make(chan ClientInterfacer),
 	}
 }
 
-func (h *Hub) Run(){
+func (h *Hub) Run() {
 	log.Println("Awaiting client registration")
-	for{
-		select{
-		case client := <- h.RegisterChan:
+	for {
+		select {
+		case client := <-h.RegisterChan:
 			client.Initialize(uint64(len(h.Clients)))
-		case client := <- h.UnregisteredChan:
+		case client := <-h.UnregisterChan:
 			h.Clients[client.Id()] = nil
-		case packet := <- h.BroadcastChan:
-			for id,client:= range h.Clients{
-				if id!= packet.SenderId{
-					client.ProcessMessage(packet,SenderId,packet.msg)
+		case packet := <-h.BroadcastChan:
+			for id, client := range h.Clients {
+				if id != packet.SenderId {
+					client.ProcessMessage(packet.SenderId, packet.Msg)
 				}
 			}
 		}
 	}
 }
 
-func (h *Hub) Serve(getNewClient func(*Hub, http.ResponseWriter, *http.Request) (ClientInterfacer, error), writer http.ResponseWriter, request *http.Request ){
-		log.Println("New Client connected from", request.RemoteAddr)
-		client, err:= getNewClient(h,writer,request)
+func (h *Hub) Serve(getNewClient func(*Hub, http.ResponseWriter, *http.Request) (ClientInterfacer, error), writer http.ResponseWriter, request *http.Request) {
+	log.Println("New Client connected from", request.RemoteAddr)
+	client, err := getNewClient(h, writer, request)
 
-		if err != nil{
-			log.Printf("Error Occurred: %v",err)
-			return
-		}
+	if err != nil {
+		log.Printf("Error Occurred: %v", err)
+		return
+	}
 
-		h.RegisterChan <- client
-		//using go keyword allows us to do a process in background thread
-		go client.WritePump()
-		go client.ReadPump()
+	h.RegisterChan <- client
+	//using go keyword allows us to do a process in background thread
+	go client.WritePump()
+	go client.ReadPump()
 }
